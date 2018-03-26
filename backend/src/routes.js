65c42997a6cd5bf1b95d1371feec9d663f3c09c6
Konstantin Lupalo <lupalo.kv@gmail.com>
2018-03-26 21:29:56 +0300

    Add ordeR module

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/users', require('./users'));
 router.use('/menu', require('./menu'));
 router.use('/orders', require('./orders'));
+router.use('/order', require('./order'));
 
 module.exports = router;
